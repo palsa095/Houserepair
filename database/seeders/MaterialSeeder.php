8ca188fc8e90ffdf4ae1681f55466ff2286d8930
sobatguruntech <sobatguruntech@gmail.com>
2025-09-21 00:10:13 +0700

    fixed bug migrate

--- database/seeders/MaterialSeeder.php
+++ database/seeders/MaterialSeeder.php
@@ -16,23 +16,35 @@ class MaterialSeeder extends Seeder
     {
         $data = [
             [
-                'survey_id' => 1,
+                'nama' => 'Pengadaan Laptop',
                 'keterangan' => 'Pengadaan 10 unit laptop untuk karyawan baru',
                 'keperluan_barang' => 'Peralatan kerja karyawan',
                 'total_harga' => 75000000
             ],
             [
-                'survey_id' => 2,
+                'nama' => 'Pembelian Printer',
                 'keterangan' => 'Printer laserjet warna untuk divisi marketing',
                 'keperluan_barang' => 'Mencetak materi promosi',
                 'total_harga' => 4500000
             ],
             [
-                'survey_id' => 3,
+                'nama' => 'Pengadaan Meja Kerja',
                 'keterangan' => 'Meja kerja ergonomis untuk ruang kerja baru',
                 'keperluan_barang' => 'Furnitur kantor',
                 'total_harga' => 12000000
-            ]
+            ],
+            [
+                'nama' => 'Pembelian Software',
+                'keterangan' => 'Lisensi software design untuk tim kreatif',
+                'keperluan_barang' => 'Alat kerja desainer',
+                'total_harga' => 8500000
+            ],
+            [
+                'nama' => 'Maintenance AC',
+                'keterangan' => 'Service rutin AC seluruh kantor',
+                'keperluan_barang' => 'Perawatan fasilitas',
+                'total_harga' => 3500000
+            ],
         ];
 
         DB::table('materials')->insert($data);
